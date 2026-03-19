1f636d9670472502b005dc8b32b944b8520be1a9
marko-lazarevic <markolazarevic37@gmail.com>
2026-03-19 21:14:11 +0100

    Add unit tests and CMakeLists for test execution

--- tests/unit/vertex_test.cpp
+++ tests/unit/vertex_test.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 #include "../../src/structs/vertex.hpp"
+#include "../../src/structs/graph.hpp"
 
 TEST_CASE("AddNeighbor stores id/weight and increments degree") {
 	Vertex v(1);
@@ -56,13 +57,56 @@ TEST_CASE("SortEdges orders neighbors and rebuilds hashes") {
 TEST_CASE("Resize keeps neighbors reachable") {
 	Vertex v(1);
 	v.AddNeighbor(2, 1);
-	v.AddNeighbor(3, 2); // triggers resize at beta/2 threshold
+	v.AddNeighbor(3, 2);
+	v.AddNeighbor(4, 1);
 
 	REQUIRE(v.Beta() == 8);
 	REQUIRE(v.IsNeighbor(2));
 	REQUIRE(v.IsNeighbor(3));
 	REQUIRE(v.GetWeight(2) == 1);
 	REQUIRE(v.GetWeight(3) == 2);
+	REQUIRE(v.GetWeight(4) == 1);
+}
+
+TEST_CASE("Duplicate insertion is ignored") {
+	Vertex v(1);
+	v.AddNeighbor(2, 1);
+	v.AddNeighbor(2, 99);
+
+	REQUIRE(v.Degree() == 1);
+	REQUIRE(v.IsNeighbor(2));
+	REQUIRE(v.GetWeight(2) == 1);
+}
+
+TEST_CASE("Neighbour iterators span exactly degree entries") {
+	Vertex v(1);
+	v.AddNeighbor(5, 1);
+	v.AddNeighbor(7, 2);
+	v.AddNeighbor(9, 3);
+
+	int count = 0;
+	for (auto it = v.NeighbourIterator(); it != v.NeighbourEndIterator(); ++it) {
+		count++;
+	}
+
+	REQUIRE(count == v.Degree());
+}
+
+TEST_CASE("RemoveNeighbor can preserve order") {
+	Vertex v(1);
+	v.AddNeighbor(2, 1);
+	v.AddNeighbor(3, 2);
+	v.AddNeighbor(4, 3);
+
+	v.RemoveNeighbor(3, true);
+
+	REQUIRE(v.Degree() == 2);
+	auto it = v.NeighbourIterator();
+	REQUIRE((it + 0)->vertex_id == 2);
+	REQUIRE((it + 1)->vertex_id == 4);
+	REQUIRE(v.IsNeighbor(2));
+	REQUIRE(v.IsNeighbor(4));
+	REQUIRE(!v.IsNeighbor(3));
 }
 
 TEST_CASE("Lots of neighbors") {
@@ -76,4 +120,21 @@ TEST_CASE("Lots of neighbors") {
         REQUIRE(v.IsNeighbor(i));
         REQUIRE(v.GetWeight(i) == i * 10);
     }
+}
+
+TEST_CASE("RemoveVertex removes incoming and reindexes IDs") {
+	Graph g;
+	g.AddVertex(); // 0
+	g.AddVertex(); // 1
+	g.AddVertex(); // 2
+
+	g.AddEdge(0, 1, 5);
+	g.AddEdge(1, 2, 6);
+	g.AddEdge(2, 1, 7);
+
+	g.RemoveVertex(1);
+
+	REQUIRE(g.VertexCount() == 2);
+	REQUIRE(!g.AreNeighbors(0, 1));
+	REQUIRE(!g.AreNeighbors(1, 0));
 }
